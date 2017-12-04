7a6faa448baedefa5a3dfbe3386bd65b1bd4bd88
Michela Zucca <michela.zucca@heig-vd.ch>
2017-12-04 16:13:08 +0100

    Création pojo et fichier mapping

---
+++ dev/src/main/java/ch/heigvd/university/Cours.java
@@ -0,0 +1,33 @@
+package ch.heigvd.university;
+
+/**
+ *
+ * @author User
+ */
+public class Cours {
+
+   private int id;
+   private String titre;
+   private int credits;
+
+   public Cours() {
+   }
+
+   public Cours(String titre, int credits) {
+      this.titre = titre;
+      this.credits = credits;
+   }
+
+   public int getCredits() {
+      return credits;
+   }
+
+   public String getTitre() {
+      return titre;
+   }
+
+   public int getId() {
+      return id;
+   } 
+   
+}
---
+++ dev/src/main/java/ch/heigvd/university/Etudiant.java
@@ -0,0 +1,33 @@
+package ch.heigvd.university;
+import java.util.Date;
+
+public class Etudiant {
+ 	private int id;
+	private String prenom;
+	private String nom;
+	private Date dateInscription;
+   
+   public Etudiant(){}
+
+   public Etudiant(String prenom, String nom, Date dateInscription) {
+      this.prenom = prenom;
+      this.nom = nom;
+      this.dateInscription = dateInscription;
+   }
+
+   public int getId() {
+      return id;
+   }
+
+   public String getNom() {
+      return nom;
+   }
+
+   public String getPrenom() {
+      return prenom;
+   }
+
+   public Date getDateInscription() {
+      return dateInscription;
+   }   
+}
