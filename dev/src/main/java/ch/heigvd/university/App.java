ca3a7a9e92651eed4be4d41acb89733cbca44d5e
Viking & Robot <vikingandrobot@gmail.com>
2017-12-04 17:24:06 +0100

    Added correct class paths, added setters to entities, configured Hibernate. Working example of Hibernate.

--- dev/src/main/java/ch/heigvd/university/App.java
+++ dev/src/main/java/ch/heigvd/university/App.java
@@ -1,10 +1,17 @@
 package ch.heigvd.university;
 
-import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 class App {
+
    public static void main(String... args) {
       System.out.println("At this moment he knew he f'd up");
+
+      SessionFactory sessionFactory = new Configuration()
+              .configure()
+              .buildSessionFactory();
+
       //Session session = HibernateUtil.getSessionFactory().openSession();
    }
 }
