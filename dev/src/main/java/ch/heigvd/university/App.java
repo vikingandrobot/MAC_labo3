76b96c13ddd0ad0f2702870accfa19884b449434
Michela Zucca <michela.zucca@heig-vd.ch>
2017-12-29 15:50:17 +0100

    Etape 3 - delete en cascade ok

--- dev/src/main/java/ch/heigvd/university/App.java
+++ dev/src/main/java/ch/heigvd/university/App.java
@@ -2,9 +2,11 @@ package ch.heigvd.university;
 
 import ch.heigvd.university.entity.Cours;
 import ch.heigvd.university.entity.Etudiant;
+import ch.heigvd.university.entity.Inscription;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import javax.persistence.TypedQuery;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -12,106 +14,166 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 class App {
-
-   private static final String[] NOMS = {"De la Vega", "Carlson", "PantHurth"};
-   private static final String[] PRENOMS = {"Marie", "Carl", "John"};
-   private static final String[] COURS = {"Bio Alchemy", "Star Flying", "Magic"};
-
-   private static SessionFactory sessionFactory;
-
-   /**
-    * Creates data in the database. Creates three students and three courses.
-    */
-   private static void createData() {
-      // Open a new session and begin a new transaction
-      List<Etudiant> etudiants = new LinkedList<>();
-      List<Cours> cours = new LinkedList<>();
-
-      Session session = sessionFactory.openSession();
-      session.beginTransaction();
-
-      // Create a bunch of students and courses
-      for (int i = 0; i < 3; ++i) {
-         etudiants.add(new Etudiant(PRENOMS[i], NOMS[i], new Date()));
-         session.save(etudiants.get(i));
-
-         cours.add(new Cours(COURS[i], (i + 1) * 2));
-         session.save(cours.get(i));
-      }
-      
-      etudiants.get(0).ajouterCours(cours.get(0));
-      etudiants.get(0).ajouterCours(cours.get(1));
-      etudiants.get(0).ajouterCours(cours.get(2));
-      etudiants.get(1).ajouterCours(cours.get(1));    
-
-      // Commit the transaction and close the session
-      session.getTransaction().commit();
-      session.close();
-   }
-
-   /**
-    * Read data from the database and display it in the console. Reads all
-    * students and courses and displays their last name or title.
-    */
-   private static void readData() {
-      // Open a new session and begin a new transaction
-      Session session = sessionFactory.openSession();
-      session.beginTransaction();
-
-      // Query the name of the students and display them
-      TypedQuery<String> query = session.createQuery("select p.nom from Etudiant as p");
-      List<String> list = query.getResultList();
-
-      System.out.println("Affichage des étudiants: ");
-      for (String nom : list) {
-         System.out.println(nom);
-      }
-
-      System.out.println("");
-
-      // Query the title of the courses and display them
-      query = session.createQuery("select c.titre from Cours as c");
-      list = query.getResultList();
-
-      System.out.println("Affichage des cours: ");
-      for (String titre : list) {
-         System.out.println(titre);
-      }
-      
-      System.out.println("Afficher les étudiants et leurs cours");
-        List listEtudiants = session.createQuery("FROM Etudiant").list();
-        for(Object e : listEtudiants){
-            System.out.println("Etudiant : " + ((Etudiant)e).getNom()+" " + ((Etudiant)e).getPrenom());
-            for(Cours c : ((Etudiant)e).getCours()){
-                System.out.println(" - cours : " + c.getTitre());
-            }       
-        }
     
-      // Commit the transaction and close the session
-      session.getTransaction().commit();
-      session.close();
-   }
+    public static void displayStudent(List<Etudiant> etudiants){
+        for (Etudiant e : etudiants) {
+            System.out.println("*** " + e.getNom()
+                    + " " + e.getPrenom());
+        }        
+    }
+    
+    public static void displayStudentAndCours(List<Etudiant> etudiants){
+          for (Etudiant e : etudiants) {
+            System.out.println("*** " + e.getNom()
+                    + " " + e.getPrenom() + "\nCours : ");
+              displayCours(e.getCours());
+        }        
+    }
+    public static void displayCours(List<Cours> cours){
+        for (Cours c : cours) {
+            System.out.println("- id:" + c.getId() + " name:" + c.getTitre() + " credits:" + c.getCredits());
+        }
+    }
+     public static void displayCours(Set<Cours> cours){
+        for (Cours c : cours) {
+            System.out.println("- id:" + c.getId() + " name:" + c.getTitre() + " credits:" + c.getCredits());
+        }
+    }
 
-   /**
-    * Main program. Creates a bunch of students and courses in the DB, and then
-    * display them back in the console after reading them from the DB.
-    *
-    * @param args
-    */
-   public static void main(String... args) {
-      try {
-         sessionFactory = new Configuration()
-                 .configure()
-                 .buildSessionFactory();
-      } catch (HibernateException ex) {
-         System.err.println("Failed to create sessionFactory object." + ex);
-         throw new ExceptionInInitializerError(ex);
-      }
+    public static void displayInscriptions(List<Inscription> inscriptions){
+     for (Inscription i : inscriptions) {
+            System.out.println(
+                    "id: " + i.getId()
+                    + " cours: <" + i.getCours().getId() + " " + i.getCours().getTitre() +">"
+                    + " etudiant: <" + i.getEtudiant().getId() + " " + i.getEtudiant().getNom()+">"
+            );
+        }
+    }
+    
+    private static final String[] NOMS = {"De la Vega", "Carlson", "PantHurth"};
+    private static final String[] PRENOMS = {"Marie", "Carl", "John"};
+    private static final String[] COURS = {"Bio Alchemy", "Star Flying", "Magic"};
+
+    private static SessionFactory sessionFactory;
+
+    /**
+     * Creates data in the database. Creates three students and three courses.
+     */
+    private static void createData() {
+        // Open a new session and begin a new transaction
+        List<Etudiant> etudiants = new LinkedList<>();
+        List<Cours> cours = new LinkedList<>();
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        // Create a bunch of students and courses
+        for (int i = 0; i < 3; ++i) {
+            etudiants.add(new Etudiant(PRENOMS[i], NOMS[i], new Date()));
+            session.save(etudiants.get(i));
+
+            cours.add(new Cours(COURS[i], (i + 1) * 2));
+            session.save(cours.get(i));
+        }
+
+        etudiants.get(0).ajouterCours(cours.get(0));
+        etudiants.get(0).ajouterCours(cours.get(1));
+        etudiants.get(0).ajouterCours(cours.get(2));
+        etudiants.get(1).ajouterCours(cours.get(0));
+        etudiants.get(1).ajouterCours(cours.get(1));
+        etudiants.get(2).ajouterCours(cours.get(2));
+        etudiants.get(2).ajouterCours(cours.get(1));
+
+        // Commit the transaction and close the session
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Read data from the database and display it in the console. Reads all
+     * students and courses and displays their last name or title.
+     */
+    private static void readData() {
+        // Open a new session and begin a new transaction
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        // Query the name of the students and display them
+        List etudiants = session.createQuery("FROM Etudiant").list();
+        
+        System.out.append("\n********** READ **********\n");
+        System.out.println("\nListe des étudiants : ");
+        displayStudent(etudiants);
       
-      // Test program that creates students and courses and then displays them
-      createData();
-      readData();
+        System.out.println("\nListe des cours : ");
+        List cours = session.createQuery("FROM Cours").list();
+        displayCours((List<Cours>)cours);
 
-      sessionFactory.close();
-   }
+        
+        System.out.println("\nListe des étudiants et leurs cours");
+        List listEtudiants = session.createQuery("FROM Etudiant").list();
+        displayStudentAndCours(etudiants);
+
+        // Commit the transaction and close the session
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    private static void deleteData() {
+        // Open a new session and begin a new transaction
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        System.out.append("\n********** DELETE **********\n");
+
+        // Query the name of the students and display them
+        List etudiants = session.createQuery("FROM Etudiant").list();
+
+        System.out.println("Etudiants avant suppression : \n");
+        displayStudent(etudiants);
+        
+        System.out.println("\nInscriptions avant suppression : ");
+        List inscriptions = session.createQuery("FROM Inscription").list();
+        displayInscriptions(inscriptions);
+
+        Etudiant etudiant = (Etudiant) etudiants.get(0);
+        System.out.println("\nSuppression de l'étudiant : " + etudiant.getId() + ". " + etudiant.getNom());
+        session.delete(etudiant);
+
+        etudiants = session.createQuery("FROM Etudiant").list();
+        System.out.println("\nEtudiants après suppression : ");
+        displayStudent(etudiants);
+        
+        System.out.println("\nInscriptions après suppression: ");
+        inscriptions = session.createQuery("FROM Inscription").list();
+        displayInscriptions(inscriptions);
+
+        // Commit the transaction and close the session
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Main program. Creates a bunch of students and courses in the DB, and then
+     * display them back in the console after reading them from the DB.
+     *
+     * @param args
+     */
+    public static void main(String... args) {
+        try {
+            sessionFactory = new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+        } catch (HibernateException ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+
+        // Test program that creates students and courses and then displays them
+        createData();
+        readData();
+        deleteData();
+
+        System.out.println("");
+        sessionFactory.close();
+    }
 }
