6055e37cd1dbe590b3640c96f8483f9c3ed5c118
Viking & Robot <vikingandrobot@gmail.com>
2018-01-21 22:49:10 +0100

    Added generation of etudiants.

--- dev/src/main/java/ch/heigvd/university/controllers/ORMAccess.java
+++ dev/src/main/java/ch/heigvd/university/controllers/ORMAccess.java
@@ -20,240 +20,217 @@ import org.hibernate.Transaction;
 import org.hibernate.HibernateException;
 
 import ch.heigvd.university.models.*;
+import java.time.LocalDate;
 
 //------------------------------------------------------------------------------
-public class ORMAccess  {
+public class ORMAccess {
 
+    @SuppressWarnings("all")
 
-   @SuppressWarnings("all") 
+    private static SessionFactory sessionFactory;
 
-   private static SessionFactory sessionFactory;
+    private static final String[] NOMS = {"De la Vega", "Carlson", "PantHurth"};
+    private static final String[] PRENOMS = {"Marie", "Carl", "John"};
+    private static final String[] COURS = {"Bio Alchemy", "Star Flying", "Magic"};
+    private static final String[] COURS_EXTERIEUR = {"Botanic", "Chirurgie", "Theatre"};
+    private static final String[] ECOLE = {"Ecole de la fleurs", "Ecole du bistouri"};
 
-   public ORMAccess() {
-      sessionFactory = new Configuration()
-            .configure()
-            .buildSessionFactory();
-   }
+    public ORMAccess() {
+        sessionFactory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
 
-   public void peuplerLaBase() throws Exception {
-      Session session = sessionFactory.openSession();
-      Transaction tx = null;
-      List<Etudiant> etudiants = null;
-      try {
-
-         tx= session.beginTransaction();
-
-         // COMPLETER A CET ENDROIT
-
-         tx.commit();
-      }
-      catch (Exception e) {
-         if (tx!= null){
-            try {
-               tx.rollback();
-               System.out.println(e.toString());
-               throw e;
-            }
-            catch (HibernateException he){
-               System.out.println(he.toString());
-               throw he;
-            }
-         }
-      }
-      finally {
-         try{
-            session.close();
-         }
-         catch (HibernateException he){
-            System.out.println(he.toString());
-         }
-      }
-   }
-
-   public List<Etudiant> GET_ETUDIANTS() throws Exception {
-      Session session = sessionFactory.openSession();
-      Transaction tx = null;
-      List<Etudiant> etudiants = null;
-      try {
-
-         tx= session.beginTransaction();
-
-         // COMPLETER A CET ENDROIT
-
-         tx.commit();
-      }
-      catch (Exception e) {
-         if (tx!= null){
-            try {
-               tx.rollback();
-               System.out.println(e.toString());
-               throw e;
-            }
-            catch (HibernateException he){
-               System.out.println(he.toString());
-               throw he;
-            }
-         }
-      }
-      finally {
-         try{
-            session.close();
-         }
-         catch (HibernateException he){
-            System.out.println(he.toString());
-         }
-      }
-
-      return etudiants;
-   }
-
-   public Etudiant GET_ETUDIANT(int studentId) throws Exception {
-      Session session = sessionFactory.openSession();
-      Transaction tx = null;
-      Etudiant etudiant = null;
-      try {
-
-         tx= session.beginTransaction();
-
-         // COMPLETER A CET ENDROIT
-
-         tx.commit();
-      }
-      catch (Exception e) {
-         if (tx!= null){
-            try {
-               tx.rollback();
-               System.out.println(e.toString());
-               throw e;
-            }
-            catch (HibernateException he){
-               System.out.println(he.toString());
-               throw he;
-            }
-         }
-      }
-      finally {
-         try{
-            session.close();
-         }
-         catch (HibernateException he){
-            System.out.println(he.toString());
-         }
-      }
-      return etudiant;
-   }
-
-   public void SAVE_ETUDIANT(Etudiant etudiant) throws Exception {
-      Session session = sessionFactory.openSession();
-      Transaction tx = null;
-      try {
-
-         tx= session.beginTransaction();
-
-         // COMPLETER A CET ENDROIT
-
-         tx.commit();
-      }
-      catch (Exception e) {
-         if (tx!= null){
-            try {
-               tx.rollback();
-               System.out.println(e.toString());
-               throw e;
-            }
-            catch (HibernateException he){
-               System.out.println(he.toString());
-               throw he;
-            }
-         }
-      }
-      finally {
-         try{
-            session.close();
-         }
-         catch (HibernateException he){
-            System.out.println(he.toString());
-         }
-      }
-   }
-
-   public void UPDATE_ETUDIANT(Etudiant etudiant) throws Exception {
-      Session session = sessionFactory.openSession();
-      Transaction tx = null;
-      try {
-
-         tx= session.beginTransaction();
-
-         // COMPLETER A CET ENDROIT
-
-         tx.commit();
-      }
-      catch (Exception e) {
-         if (tx!= null){
-            try {
-               tx.rollback();
-               System.out.println(e.toString());
-               throw e;
-            }
-            catch (HibernateException he){
-               System.out.println(he.toString());
-               throw he;
-            }
-         }
-      }
-      finally {
-         try{
-            session.close();
-         }
-         catch (HibernateException he){
-            System.out.println(he.toString());
-         }
-      }
-   }
-
-  public void DELETE_ETUDIANT(int studentId) throws Exception {
-      Session session = sessionFactory.openSession();
-      Transaction tx = null;
-      try {
-
-         tx= session.beginTransaction();
-
-         // COMPLETER A CET ENDROIT
-
-         tx.commit();
-      }
-      catch (Exception e) {
-         if (tx!= null){
-            try {
-               tx.rollback();
-               System.out.println(e.toString());
-               throw e;
-            }
-            catch (HibernateException he){
-               System.out.println(he.toString());
-               throw he;
-            }
-         }
-      }
-      finally {
-         try{
-            session.close();
-         }
-         catch (HibernateException he){
-            System.out.println(he.toString());
-         }
-      }
-   }
-
-   public static void terminate() {
+    public void peuplerLaBase() throws Exception {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
         try {
-            if ( sessionFactory != null ) {
-               sessionFactory.close();
+
+            tx = session.beginTransaction();
+
+            for (int i = 0; i < 3; ++i) {
+                session.save(new Etudiant(PRENOMS[i], NOMS[i], LocalDate.now()));
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                    System.out.println(e.toString());
+                    throw e;
+                } catch (HibernateException he) {
+                    System.out.println(he.toString());
+                    throw he;
+                }
+            }
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                System.out.println(he.toString());
             }
         }
-        catch(Exception e) {
-            System.out.println (e.toString());
-        }      
-   }
+    }
+
+    public List<Etudiant> GET_ETUDIANTS() throws Exception {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        List<Etudiant> etudiants = null;
+        try {
+
+            tx = session.beginTransaction();
+
+            // COMPLETER A CET ENDROIT
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                    System.out.println(e.toString());
+                    throw e;
+                } catch (HibernateException he) {
+                    System.out.println(he.toString());
+                    throw he;
+                }
+            }
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                System.out.println(he.toString());
+            }
+        }
+
+        return etudiants;
+    }
+
+    public Etudiant GET_ETUDIANT(int studentId) throws Exception {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        Etudiant etudiant = null;
+        try {
+
+            tx = session.beginTransaction();
+
+            // COMPLETER A CET ENDROIT
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                    System.out.println(e.toString());
+                    throw e;
+                } catch (HibernateException he) {
+                    System.out.println(he.toString());
+                    throw he;
+                }
+            }
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                System.out.println(he.toString());
+            }
+        }
+        return etudiant;
+    }
+
+    public void SAVE_ETUDIANT(Etudiant etudiant) throws Exception {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+
+            tx = session.beginTransaction();
+
+            // COMPLETER A CET ENDROIT
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                    System.out.println(e.toString());
+                    throw e;
+                } catch (HibernateException he) {
+                    System.out.println(he.toString());
+                    throw he;
+                }
+            }
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                System.out.println(he.toString());
+            }
+        }
+    }
+
+    public void UPDATE_ETUDIANT(Etudiant etudiant) throws Exception {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+
+            tx = session.beginTransaction();
+
+            // COMPLETER A CET ENDROIT
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                    System.out.println(e.toString());
+                    throw e;
+                } catch (HibernateException he) {
+                    System.out.println(he.toString());
+                    throw he;
+                }
+            }
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                System.out.println(he.toString());
+            }
+        }
+    }
+
+    public void DELETE_ETUDIANT(int studentId) throws Exception {
+        Session session = sessionFactory.openSession();
+        Transaction tx = null;
+        try {
+
+            tx = session.beginTransaction();
+
+            // COMPLETER A CET ENDROIT
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                try {
+                    tx.rollback();
+                    System.out.println(e.toString());
+                    throw e;
+                } catch (HibernateException he) {
+                    System.out.println(he.toString());
+                    throw he;
+                }
+            }
+        } finally {
+            try {
+                session.close();
+            } catch (HibernateException he) {
+                System.out.println(he.toString());
+            }
+        }
+    }
+
+    public static void terminate() {
+        try {
+            if (sessionFactory != null) {
+                sessionFactory.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 
 }
